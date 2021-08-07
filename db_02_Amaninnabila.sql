6fbf31296bac75a6a2f027c348819a8b885f3d70
Amaninnabilaananda <88481785+Amaninnabilaananda@users.noreply.github.com>
2021-08-07 16:50:32 +0700

    Update db_02_Amaninnabila.sql

--- db_02_Amaninnabila.sql
+++ db_02_Amaninnabila.sql
@@ -1,16 +1,15 @@
-#tugas_pweb
- -- phpMyAdmin SQL Dump
- -- versi 5.0.2
- -- https://www.phpmyadmin.net/
- --
- -- Tuan rumah: 127.0.0.1
- -- Waktu Pembuatan: 07 Agustus 16:39
- -- Versi server: 10.4.14-MariaDB
- -- Versi PHP: 7.2.33
+-- phpMyAdmin SQL Dump
+-- versi 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Tuan rumah: 127.0.0.1
+-- Waktu pembuatan: 07 Agu 2021 pada 16.50
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 7.4.21
 
- SET SQL_MODE =  " NO_AUTO_VALUE_ON_ZERO " ;
- MULAI TRANSAKSI ;
- SET zona_waktu =  " +00:00 " ;
+SET SQL_MODE =  " NO_AUTO_VALUE_ON_ZERO " ;
+MULAI TRANSAKSI ;
+SET zona_waktu =  " +00:00 " ;
 
 
 /* !40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */ ;
@@ -19,26 +18,26 @@
 /* !40101 SET NAMA utf8mb4 */ ;
 
 --
--- Database: `db_02_Amaninnabila`
+-- Basis data: `db_02_amaninnabila`
 --
 
 - ------------------------------------------------ --------
 
 --
--- Struktur tabel untuk tabel `tb_siswa`
+-- Struktur dari tabel `tb_siswa`
 --
 
 CREATE  TABLE ` tb_siswa` (
   ` Id_siswa `  int ( 11 ) NOT NULL ,
   ` Nama di `  varchar ( 100 ) NOT NULL ,
-  ` hp `  int ( 20 ) TIDAK NULL
+  ` No_hp `  int ( 15 ) NOT NULL
 ) ENGINE = CHARSET DEFAULT InnoDB = utf8mb4;
 
 --
 -- Dumping data untuk tabel `tb_siswa`
 --
 
-INSERT INTO  ` tb_siswa ` ( ` id_siswa ` , ` nama di ` , ` hp ` ) VALUES
+INSERT INTO  ` tb_siswa ` ( ` id_siswa ` , ` nama di ` , ` no_hp ` ) VALUES
 ( 2 , ' Amanin Nabila Ananda ' , 265474 );
 
 --
@@ -59,9 +58,9 @@ ALTER  TABLE  ` tb_siswa `
 -- AUTO_INCREMENT untuk tabel `tb_siswa`
 --
 ALTER  TABLE  ` tb_siswa `
-  MENGUBAH ` id_siswa `  int ( 11 ) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 5 ;
+  MENGUBAH ` id_siswa `  int ( 11 ) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 24 ;
 BERKOMITMEN ;
 
 /* !40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */ ;
 /* !40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */ ;
-/* !40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION
+/* !40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */ ;
